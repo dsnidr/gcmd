ea1b32ef8068f0afce98f581d6a7252baa5ee9eb
Sniddunc <sniddunc2@gmail.com>
2020-08-04 22:24:16 -0400

    :tada: Initial version

---
+++ base.go
@@ -0,0 +1,109 @@
+package gcmd
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Base is our command base. It keeps track of all commands and their handlers
+type Base struct {
+	CommandSymbol         string
+	Commands              map[string]Command
+	UnknownCommandMessage string
+}
+
+// New is the function for creating a new command base. Please use this function instead of creating an instance of Base yourself!
+//
+// If you really must create a Base yourself, then make sure that CommandSymbol and UnknownCommandMessage are set, and that
+// you initialize the Commands map yourself or else you will run into some issues!
+func New(commandSymbol string) Base {
+	return Base{
+		CommandSymbol:         commandSymbol,
+		UnknownCommandMessage: "Unknown command",
+		Commands:              make(map[string]Command),
+	}
+}
+
+// CommandExists checks if a command is registered. It returns true if it is, false if it isn't.
+// CommandExists compares the Name fields of commands.
+func (base *Base) CommandExists(command string) bool {
+	if _, ok := base.Commands[command]; ok {
+		return true
+	}
+
+	return false
+}
+
+// GetCommand retrieves a Command by it's name field from the command base.
+//
+// GetCommand returns a Command and a boolean. This boolean is true if a command was found, and false if
+// a command could not be found with the provided name.
+func (base *Base) GetCommand(command string) (Command, bool) {
+	if cmd, ok := base.Commands[command]; ok {
+		return cmd, ok
+	}
+
+	return Command{}, false
+}
+
+// Register takes in a command string and a command handler function (type Handler).
+//
+// CommandExists is used to check if the command is already registered or not. If the command is
+// already registered, an error will be returned.
+func (base *Base) Register(command Command) error {
+	if base.CommandExists(command.Name) {
+		return fmt.Errorf("A command with that name already exists")
+	}
+
+	base.Commands[command.Name] = command
+
+	return nil
+}
+
+// HandleCommand is called to run what is assumed to be a command.
+//
+// HandlerCommand returns a boolean and an error. The boolean is true if the input provided was structed as a proper command
+// meaning that it was a string whose first character is equal to this base's CommandSymbol. If it was not a structured command
+// the bool will be false and the returned error will be nil. In this case, you should assume the user was not trying to execute
+// a command.
+//
+// If the user entered a properly structured command, but it was either an unknown command or if the input failed validation
+// then the bool will be false and error will be set to an error.
+//
+// If the command was fully executed successfully, the bool will be true and error will be nil.
+func (base *Base) HandleCommand(input string) (bool, error) {
+	input = strings.TrimSpace(input)
+
+	// If the first char wasn't equal to base.CommandSymbol, we assume that the user was not trying to execute a command
+	// so we return false and do not set an error.
+	if input[0:1] != base.CommandSymbol {
+		return false, nil
+	}
+
+	// Now that we know this is structured as a command, we can strip away the CommandSymbol char.
+	input = input[1:]
+
+	// Split the input by spaces to get all provided arguments
+	args := strings.Split(input, " ")
+
+	// Check if this command exists and retrieve it if it does. If it doesn't exist, return base.UnknownCommandMessage
+	command, ok := base.GetCommand(args[0])
+	if !ok {
+		return false, fmt.Errorf(base.UnknownCommandMessage)
+	}
+
+	// Remove the command identifier from args
+	args = args[1:]
+
+	// Run all command validators
+	if valid, err := command.Validate(args); !valid {
+		return false, err
+	}
+
+	// If validation passed, then now we run the command handler.
+	if success := command.Handler(args); !success {
+		return false, nil
+	}
+
+	return true, nil
+}
---
+++ base_test.go
@@ -0,0 +1,100 @@
+package gcmd
+
+import (
+	"fmt"
+	"testing"
+)
+
+var command1 = Command{
+	Name:       "give",
+	Usage:      "give <player> <thing>",
+	Validators: []Validator{command1Validator1, command1Validator2},
+	Handler:    command1Handler,
+}
+
+func command1Validator1(args []string) error {
+	if len(args) != 2 {
+		return fmt.Errorf("Not enough arguments. Expected 2, received %d", len(args))
+	}
+
+	return nil
+}
+
+func command1Validator2(args []string) error {
+	if args[1] != "job" {
+		return fmt.Errorf("Job not offered. Expected: \"job\", received %s", args[1])
+	}
+
+	return nil
+}
+
+func command1Handler(args []string) bool {
+	// We have nothing we actually want to do here, but this is normally where you'd write your code.
+	// For example, inserting something into a database, terminating a connection, etc.
+	return true
+}
+
+func TestRegisterCommand(t *testing.T) {
+	base := New("/")
+
+	if err := base.Register(command1); err != nil {
+		t.Fatalf("Register should've returned nil, but returned an error: %v", err)
+	}
+}
+
+func TestHandleCommandPass(t *testing.T) {
+	base := New("/")
+
+	if err := base.Register(command1); err != nil {
+		t.Fatalf("Register should've returned nil, but returned an error: %v", err)
+	}
+
+	// Should run successfully
+	input := "/give sniddunc job"
+	ok, err := base.HandleCommand(input)
+	if !ok || err != nil {
+		t.Fatalf("Command should've run successfully, but failed. Ok: %v, Error: %v", ok, err)
+	}
+}
+
+func TestHandleCommandFail(t *testing.T) {
+	base := New("/")
+
+	if err := base.Register(command1); err != nil {
+		t.Fatalf("Register should've returned nil, but returned an error: %v", err)
+	}
+
+	// Should fail due to lack of arguments
+	input := "/give"
+	ok, err := base.HandleCommand(input)
+	if ok || err == nil {
+		t.Fatalf("Command should've failed, but succeeded. Ok: %v, Error: %v", ok, err)
+	}
+
+	// Should fail due to unknown command
+	input = "/unknown"
+	ok, err = base.HandleCommand(input)
+	if ok || err == nil {
+		t.Fatalf("Command should've failed, but succeeded. Ok: %v, Error: %v", ok, err)
+	}
+}
+
+func TestUnknownCommandMessage(t *testing.T) {
+	base := New("/")
+
+	// Should return default unknown command message
+	input := "/unknown"
+	ok, err := base.HandleCommand(input)
+	if ok || err.Error() != "Unknown command" {
+		t.Fatalf("Default unknown command message should've been returned, but wasn't. Ok: %v, Error: %v", ok, err)
+	}
+
+	// Should return custom unknown command message
+	customMessage := "Not sure why this test exists, if I'm being honest. I do enjoy testing, though."
+	base.UnknownCommandMessage = customMessage
+	input = "/unknown"
+	ok, err = base.HandleCommand(input)
+	if ok || err.Error() != customMessage {
+		t.Fatalf("Custom unknown command message should've been returned, but wasn't. Ok: %v, Error: %v", ok, err)
+	}
+}
